08a9074dc3579e695e6575b59115f023370e2d6e
AnhGoDev <anhnguyendinh263@gmail.com>
2025-08-07 22:58:06 +0700

    initial source

---
+++ Tikluy/View/Auth/Login.swift
@@ -0,0 +1,18 @@
+//
+//  Login.swift
+//  Tikluy
+//
+//  Created by Đình Anh Nguyễn  on 7/8/25.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Login()
+}
