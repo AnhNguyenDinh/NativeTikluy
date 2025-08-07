ad3cefeb7345249cbecee60287932e46096ef6e7
AnhGoDev <anhnguyendinh263@gmail.com>
2025-08-07 21:27:13 +0700

    Initial Commit

---
+++ Tikluy/TikluyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikluyApp.swift
+//  Tikluy
+//
+//  Created by Đình Anh Nguyễn  on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct TikluyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
