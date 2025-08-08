e3078a2614f8ebf50fa2f2a8ec7f8e12db365bc1
AnhGoDev <anhnguyendinh263@gmail.com>
2025-08-08 16:21:18 +0700

    update

---
+++ Tikluy/LoadingScreen.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Tikluy
+//
+//  Created by Đình Anh Nguyễn  on 7/8/25.
+//
+
+import SwiftUI
+
+struct LoadingScreen: View {
+    @State private var isNavigateToLogin: Bool = false
+    var body: some View {
+        NavigationView{
+            ZStack {
+                ProgressView()
+                    .scaleEffect(2)
+                    .tint(.red)
+                    .accentColor(.blue)
+                NavigationLink("",destination:Login() , isActive: $isNavigateToLogin).hidden()
+            }
+            .frame(maxWidth: .infinity,  maxHeight: .infinity )
+            .background(
+                Image("bg_new_home")
+                    .resizable(resizingMode: .stretch)
+                    .scaledToFill()
+            )
+            .ignoresSafeArea(.all)
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                isNavigateToLogin = true
+            }
+        }
+        .onDisappear{
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                isNavigateToLogin = true
+            }
+        }
+    
+    }
+}
+
+#Preview {
+    LoadingScreen()
+}
--- Tikluy/View/Auth/Login.swift
+++ Tikluy/View/Auth/Login.swift
@@ -15,58 +15,76 @@ let heightLogo =  widthLogo / 1.16
 struct Login: View {
     @State private var username: String = ""
     @State private var password: String = ""
-  
+    @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        GeometryReader{geometry in
-            let safeAreaTop = geometry.safeAreaInsets.top
-            ZStack {
-                ScrollView{
-                    VStack {
-                        Spacer().frame(height:screenWidth * 0.2)
-                        Image("logo_small")
-                            .resizable()
-                            .frame(width: widthLogo, height:heightLogo)
-                        Spacer().frame(height:40)
-                        TextField("Số điện thoại", text: $username)
-                            .padding(.vertical,0)
-                            .onChange(of: username) { oldValue, newValue in
-                                print("\(oldValue)- \(newValue)")
-                            }
+        NavigationView{
+            GeometryReader{geometry in
+                let safeAreaTop = geometry.safeAreaInsets.top
+                ZStack {
+                    ScrollView{
+                        VStack {
+                            Spacer().frame(height:screenWidth * 0.2)
+                            Image("logo_small")
+                                .resizable()
+                                .frame(width: widthLogo, height:heightLogo)
+                            Spacer().frame(height:40)
+                            TextField("Số điện thoại", text: $username)
+                                .padding(.vertical,0)
+                                .onChange(of: username) { oldValue, newValue in
+                                    print("\(oldValue)- \(newValue)")
+                                }
                             
-                        Rectangle() // Đây là viền dưới
-                                       .fill(Color.gray)
-                                       .frame(height: 1)
-                        Spacer().frame(height:40)
-                        SecureField("Mật khẩu",text: $password)
-                 
-        
-                        
+                            Rectangle() // Đây là viền dưới
+                                .fill(Color.gray)
+                                .frame(height: 1)
+                            Spacer().frame(height:40)
+                            SecureField("Mật khẩu",text: $password)
+                            Spacer().frame(height:20)
+                            HStack{
+                                Spacer()
+                                Button {
+                                    dismiss()
+                                } label: {
+                                    Text("Quên mật khẩu?").frame( maxWidth:.infinity,alignment: .trailing )
+                                        .foregroundColor(.black)
+                                        .underline(true, color:.black)
+                                }
+
+                                
+                            }
+                               
+                            
+                            
+                            
+                        }
+                        .padding(.horizontal, 40)
+                        //                .frame(maxWidth: .infinity, minHeight: screenHeight)
                     }
-                    .padding(.horizontal, 40)
-    //                .frame(maxWidth: .infinity, minHeight: screenHeight)
+                    .padding(.top, safeAreaTop)
+                    
+                    
+                    
                 }
-                .padding(.top, safeAreaTop)
-                
-                
-            
+                .frame(maxWidth: .infinity,  maxHeight: .infinity )
+                .background(
+                    Image("bg_new_home")
+                        .resizable(resizingMode: .stretch)
+                        .scaledToFill()
+                )
+                .ignoresSafeArea(.all)
             }
-            .frame(maxWidth: .infinity,  maxHeight: .infinity )
-            .background(
-                Image("bg_new_home")
-                    .resizable(resizingMode: .stretch)
-                    .scaledToFill()
-            )
-            .ignoresSafeArea(.all)
+            .onAppear {
+                username = "0357628024"
+                password = "Admin123@"
+            }
         }
-        .onAppear {
-            username = "0357628024"
-            password = "Admin123@"
-        }
+        .navigationBarHidden(true)
         
-    
+        
+        
     }
-        
+    
 }
 
 
