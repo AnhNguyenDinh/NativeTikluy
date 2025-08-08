729977703611e83fa84ffb0e9b5f75dc7cb1fe02
AnhGoDev <anhnguyendinh263@gmail.com>
2025-08-08 23:39:38 +0700

    update

---
+++ Tikluy/Components/TextFieldCustom.swift
@@ -0,0 +1,60 @@
+//
+//  TextFieldCustom.swift
+//  Tikluy
+//
+//  Created by Đình Anh Nguyễn  on 8/8/25.
+//
+
+import SwiftUI
+
+struct TextFieldCustom: View {
+        var label: String
+        var placeholder: String
+        @Binding var text: String
+        var icon: String? = nil ?? "phone"
+        var maxLength: Int = 5
+        var isSecure: Bool =  false
+        @FocusState private var focusState :Bool
+    
+    
+        var body: some View {
+            VStack(alignment: .leading, spacing: 6) {
+                Text(label)
+                    .font(.system(size: 12, weight: .medium))
+                    .foregroundColor(.gray)
+                
+                HStack {
+                    if let icon = icon {
+                        Image(icon)
+                            .frame(width: 30, height: 30, alignment: Alignment.center)
+//                            .foregroundColor(.gray)
+                    }
+                    
+                    VStack{
+                        if !isSecure {
+                            TextField(placeholder, text: $text)
+                                .focused($focusState)
+                                
+                        }
+                        else{
+                            SecureField(placeholder,text: $text)
+                                .focused($focusState)
+                        }
+                        Rectangle() // Đây là viền dưới
+                            .fill(focusState ? Color.black : Color.gray)
+                            .frame(height: 2)
+                    }
+//                    TextField(placeholder, text: $text)
+                }
+//                .padding(.horizontal, 10)
+//                Rectangle() // Đây là viền dưới
+//                    .fill(Color.gray)
+//                    .frame(height: 1)
+               
+            }
+        }
+}
+
+#Preview {
+    TextFieldCustom(label: "Label", placeholder: "", text: .constant(""))
+}
--- Tikluy/View/Auth/Login.swift
+++ Tikluy/View/Auth/Login.swift
@@ -9,57 +9,124 @@ import SwiftUI
 let screenWidth = UIScreen.main.bounds.width
 let screenHeight = UIScreen.main.bounds.height
 
-let widthLogo = screenWidth * 0.3
-let heightLogo =  widthLogo / 1.16
+let widthLogo = screenWidth * 0.5
+let heightLogo =  widthLogo / 4
 
 struct Login: View {
     @State private var username: String = ""
     @State private var password: String = ""
-    @Environment(\.dismiss) var dismiss
+    @State private var isLogin: Bool = true
     
     var body: some View {
         NavigationView{
             GeometryReader{geometry in
                 let safeAreaTop = geometry.safeAreaInsets.top
+               
                 ZStack {
                     ScrollView{
+                        Spacer().frame(height:screenWidth * 0.2)
+                        Image("logo_full")
+                            .resizable()
+                            .frame(width: widthLogo, height:heightLogo)
+                        
+                        Spacer().frame(height:40)
                         VStack {
-                            Spacer().frame(height:screenWidth * 0.2)
-                            Image("logo_small")
-                                .resizable()
-                                .frame(width: widthLogo, height:heightLogo)
-                            Spacer().frame(height:40)
-                            TextField("Số điện thoại", text: $username)
-                                .padding(.vertical,0)
-                                .onChange(of: username) { oldValue, newValue in
-                                    print("\(oldValue)- \(newValue)")
-                                }
+                            if isLogin {
+                                Text("ĐĂNG NHẬP")
+//                                    .fontWeight(.bold)
+                                    .font(.system(size: 20, weight: .bold, design: .default))
+                                VStack{}.frame( width:screenWidth * 0.1,height:2).background(.black)
+                            }
+                                             
                             
-                            Rectangle() // Đây là viền dưới
-                                .fill(Color.gray)
-                                .frame(height: 1)
+                            
+                            TextFieldCustom(label: "Số điện thoại", placeholder: "", text: $username,icon: "phone")
+                                .onChange(of: username) { oldValue, newValue in
+                                    username = newValue
+                                }
+                                .keyboardType(.numberPad)
+                            
+                            
                             Spacer().frame(height:40)
-                            SecureField("Mật khẩu",text: $password)
+                            TextFieldCustom(label: "Mật khẩu", placeholder: "", text: $password,icon: "password", isSecure: true)
+                                .onChange(of: password) { oldValue, newValue in
+                                    password = newValue
+                                }
                             Spacer().frame(height:20)
                             HStack{
                                 Spacer()
                                 Button {
-                                    dismiss()
+                                    
                                 } label: {
                                     Text("Quên mật khẩu?").frame( maxWidth:.infinity,alignment: .trailing )
                                         .foregroundColor(.black)
                                         .underline(true, color:.black)
+                                        .font(.system(size: 14, weight: .semibold, design: .default))
                                 }
 
                                 
                             }
-                               
-                            
-                            
-                            
+                            Spacer().frame(height:40)
+                            HStack(){
+                                Button {
+//                                    print("Đăng ký")
+                                } label: {
+                                    Text("Đăng ký")
+                                        .padding(.horizontal,16)
+                                        .padding(.vertical,12)
+                                        .fontWeight(.semibold)
+                                        .foregroundColor(.red)
+                                        .frame(width:screenWidth * 0.4)
+                                        .background(Color.white)
+                                        .cornerRadius(4)
+                                        .overlay(
+                                                RoundedRectangle(cornerRadius: 4)
+                                                    .stroke(Color.red, lineWidth: 1)
+                                            )
+                                        .shadow(radius: 2)
+                                        
+                                        
+                                        
+                                }
+                                Spacer()
+                                Button {
+                                    print("Đăng nhập \(username) \(password)")
+                                } label: {
+                                    Text("Đăng nhập")
+                                        .padding(.horizontal,16)
+                                        .padding(.vertical,12)
+                                        .fontWeight(.semibold)
+                                        .foregroundColor(.white)
+                                        .frame(width:screenWidth * 0.4)
+                                        .background(Color.red)
+                                        .cornerRadius(4)
+                                        .shadow(radius: 2)
+                                        
+                                        
+                                        
+                                }
+
+                            }
+                            Spacer().frame(height:20)
+                            VStack{
+                                Button {
+//                                    print("Đăng nhập")
+                                } label: {
+                                    Image("face_id")
+                                        .resizable()
+                                        .frame(width: 80, height: 80)
+                                        
+                                        
+                                        
+                                }
+                            }
                         }
-                        .padding(.horizontal, 40)
-                        //                .frame(maxWidth: .infinity, minHeight: screenHeight)
+                        
+                        .padding(.all, 20)
+                        .background(Color.white.opacity(0.2)
+)
+                        .cornerRadius(20)
+                        .padding(.horizontal, 20)
                     }
                     .padding(.top, safeAreaTop)
                     
@@ -74,15 +141,13 @@ struct Login: View {
                 )
                 .ignoresSafeArea(.all)
             }
-            .onAppear {
-                username = "0357628024"
-                password = "Admin123@"
-            }
+           
         }
         .navigationBarHidden(true)
-        
-        
-        
+        .onAppear {
+//                username = "123"
+//                password = "123"
+        }
     }
     
 }
